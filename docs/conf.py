e6016f000e883c448fafb881598004ceb61f87e8
Christoph Zwerschke <cito@online.de>
2019-12-29 13:11:03 +0100

    Add the old logo to the documentation

--- docs/conf.py
+++ docs/conf.py
@@ -81,4 +81,4 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = ['_static']
-html_static_path = []
+html_static_path = ['css']
