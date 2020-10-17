12aab3cc002ba286c34cbcb622f36c91a5466abe
Christoph Zwerschke <cito@online.de>
2020-10-17 19:34:00 +0200

    Bump version number for final release

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = 'Christoph Zwerschke et al.'
 # The short X.Y version
 version = '3.0'
 # The full version, including alpha/beta/rc tags
-release = '3.0.0b0'
+release = '3.0.0'
 
 
 # -- General configuration ---------------------------------------------------
--- webware/Properties.py
+++ webware/Properties.py
@@ -1,6 +1,6 @@
 name = 'Webware for Python'
 
-version = (3, 0, 0, 'b0')
+version = (3, 0, 0)
 
 status = 'stable'
 
