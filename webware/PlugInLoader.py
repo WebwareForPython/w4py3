88635c16170596eae8a24a459411d20dd017497a
Jason Hildebrand <jason@peaceworks.ca>
2022-03-10 15:12:41 -0600

    Fix logic error in plugin loader (#13)
    The logic should be same with or without verbose mode.

--- webware/PlugInLoader.py
+++ webware/PlugInLoader.py
@@ -53,12 +53,12 @@ class PlugInLoader:
             if name in plugIns:
                 if verbose:
                     print(f'Plug-in {name} has already been loaded.')
-                    continue
+                continue
             entry_point = entryPoints.get(name)
             if not entry_point:
                 if verbose:
                     print(f'Plug-in {name} has not entry point.')
-                    continue
+                continue
             module = entry_point.load()
             plugIn = self.loadPlugIn(name, module, verbose=verbose)
             if plugIn:
